19055548d3c1d3ed271bda0d01328d3dc5d1b793
Germán Medaglia <german.medaglia@gmail.com>
2026-02-02 13:58:22 -0300

    Use explicit null type on parameters.

--- src/Exceptions/NoSlotAvailableException.php
+++ src/Exceptions/NoSlotAvailableException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class NoSlotAvailableException extends RuntimeException
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    public function __construct($message = "", $code = 0, ?Throwable $previous = null)
     {
         parent::__construct("There is no parking slot available.", $code, $previous);
     }
